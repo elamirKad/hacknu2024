afea3cf137792d0dffb7499a714fb60790b0a920
elamirkad <elamirkad@gmail.com>
2024-04-13 04:30:15 +0500

    add auth swagger

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 from .serializers import UserSerializer, UserLoginSerializer
 
 
 class CreateUserView(APIView):
+    @swagger_auto_schema(request_body=UserSerializer, responses={201: UserSerializer})
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -15,6 +17,7 @@ class CreateUserView(APIView):
 
 
 class LoginView(APIView):
+    @swagger_auto_schema(request_body=UserLoginSerializer, responses={200: UserLoginSerializer})
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
