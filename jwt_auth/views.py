79f25844f79a79e8164244dd029a4c6bc0f33190
elamirkad <elamirkad@gmail.com>
2024-04-13 04:42:38 +0500

    fix login

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -20,21 +20,10 @@ class CreateUserView(APIView):
 
 
 class LoginView(APIView):
-    @swagger_auto_schema(request_body=UserLoginSerializer, responses={200: UserSerializer()})
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
-            username = serializer.validated_data['username']
-            password = serializer.validated_data['password']
-            user = authenticate(username=username, password=password)
-            if user:
-                refresh = RefreshToken.for_user(user)
-                data = {
-                    'user': UserSerializer(user).data,
-                    'refresh': str(refresh),
-                    'access': str(refresh.access_token),
-                }
-                return Response(data, status=status.HTTP_200_OK)
-            else:
-                return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            data = serializer.validated_data
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
