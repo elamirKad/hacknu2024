174f47cefda0c8fe84a25f4b3daeab77c6e989b4
elamirkad <elamirkad@gmail.com>
2024-04-13 04:40:04 +0500

    fix login

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -24,8 +24,8 @@ class LoginView(APIView):
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
-            username = serializer.validated_data.get('username')
-            password = serializer.validated_data.get('password')
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
             user = authenticate(username=username, password=password)
             if user:
                 refresh = RefreshToken.for_user(user)
