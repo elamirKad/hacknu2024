fbf3215b0e220a6e4ad8101698d81f98a0620aae
elamirkad <elamirkad@gmail.com>
2024-04-13 23:50:04 +0500

    fix cors headers

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from corsheaders.defaults import default_headers
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +41,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://*.ngrok-free.app',
 ]
 
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'ngrok-skip-browser-warning',
+]
 
 
 # Application definition
