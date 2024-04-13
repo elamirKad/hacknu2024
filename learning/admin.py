04b8929776cb1c232904b461af1e93d5a56d04fc
elamirkad <elamirkad@gmail.com>
2024-04-13 12:35:21 +0500

    add admin panel for level

--- learning/admin.py
+++ learning/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Experience
 
-# Register your models here.
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reading_exp', 'speaking_exp', 'grammar_exp', 'display_total_experience', 'display_total_level')
+    search_fields = ('user__username',)
+    list_filter = ('user__username',)
+
+    def display_total_experience(self, obj):
+        return obj.total_experience
+    display_total_experience.short_description = 'Total Experience'
+
+    def display_total_level(self, obj):
+        level, title = obj.total_level
+        return format_html('<b>{}</b> - {}', level, title)
+    display_total_level.short_description = 'Total Level'
+
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'reading_exp', 'speaking_exp', 'grammar_exp')
+        }),
+        ('Levels', {
+            'fields': (),
+            'description': 'The following sections are computed based on the experience values above.'
+        }),
+        ('Computed Levels', {
+            'fields': ('display_total_experience', 'display_total_level'),
+            'classes': ('collapse',)
+        }),
+    )
+
+    readonly_fields = ('display_total_experience', 'display_total_level')
+
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.has_perm('learning.change_experience'):
+            return self.readonly_fields + ('reading_exp', 'speaking_exp', 'grammar_exp')
+        return self.readonly_fields
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
